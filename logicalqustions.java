ce54f55b1779f073d74890e795fcf216a4f4b80b
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-10-02 23:12:20 +0530

    Add files via upload

---
+++ logicalqustions.java
@@ -0,0 +1,15 @@
+package selfpratices;
+
+public class logicalqustions 
+{
+	public static void  main(String []args)
+	{
+		int  x=12;
+		int a;
+		if(x<15)
+			a=5;
+		System.out.println(a);
+	}
+
+}
+////complotaion time errors
