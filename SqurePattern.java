05d41f7e70369a2d95a36208450f2583db7385f7
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-10-02 23:13:03 +0530

    Add files via upload

---
+++ SqurePattern.java
@@ -0,0 +1,19 @@
+package selfpratices;
+
+public class SqurePattern
+{
+	public static void main(String []args)
+	{
+		// outer loop
+		for(int i=1; i<=4; i++)
+		{
+			for(int j=1; i<=4; j++)// inner loop
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+	}
+
+}
