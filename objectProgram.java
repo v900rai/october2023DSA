4564f69737be9a6d08eaa0432a30bc3a3b79c6ba
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-10-05 16:05:45 +0530

    Add files via upload

---
+++ objectProgram.java
@@ -0,0 +1,19 @@
+package selfpratices;
+
+public class objectProgram 
+{
+	public static void main(String []args)
+	{
+		String x=m1(10.1,12.1f);
+		System.out.println(x);
+		
+	}
+	public static String m1(double a,float b) 
+	{
+	
+		System.out.println(a);
+		System.out.println(b);
+		return "vishal rai";
+	}
+
+}
