f8de0e7213440432c31bbab0d4ddd0cf4a36cd96
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-10-09 23:50:01 +0530

    Add files via upload

---
+++ oddevenprogram.java
@@ -0,0 +1,18 @@
+package DSA;
+
+public class oddevenprogram 
+{
+	public static void main(String []args)
+	{
+		int num=19;
+		if(num%2==0)
+		{
+			System.out.println("Even number");
+		}
+		else
+		{
+			System.out.println("odd number");
+		}
+	}
+
+}
