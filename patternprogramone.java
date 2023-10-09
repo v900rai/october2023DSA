4f3c7ba0d15c4c9c81bbb55cde6d56c9bf001a44
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-10-09 23:50:42 +0530

    Add files via upload

---
+++ patternprogramone.java
@@ -0,0 +1,24 @@
+package DSA;
+
+public class patternprogramone 
+{
+	public static void main(String args[])
+	{
+		
+	
+	int num=5;
+	int k=1;
+		for(int i=1; i<=num; i++)
+		{
+			for(int j=1; i<=num; j++)
+			{
+				System.out.print(k);
+				k++;
+				
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
