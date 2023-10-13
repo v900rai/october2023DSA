1c440d53b9b285bad6e1394917e35ce70c3eb495
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-10-13 23:02:06 +0530

    Add files via upload

---
+++ methodParameters.java
@@ -0,0 +1,15 @@
+package october100;
+
+public class methodParameters 
+{
+	static void myMethod(String fname) {
+	    System.out.println(fname + " Refsnes");
+	  }
+
+	  public static void main(String[] args) {
+	    myMethod("Liam");
+	    myMethod("Jenny");
+	    myMethod("Anja");
+	  }
+
+}
