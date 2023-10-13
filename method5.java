2cc06d20bd8b25dd5424697ab0e4981936c0f2eb
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-10-14 00:31:50 +0530

    Add files via upload

---
+++ method5.java
@@ -0,0 +1,18 @@
+package october100;
+
+public class method5 
+{
+	 static int myMethod(int x) {
+	    return 5 + x;
+	  }
+	static float myMethodt(float y) {
+	return 6+y;
+	}
+
+	  public static void main(String[] args) 
+	  {
+	    System.out.println(myMethod(3));
+	    System.out.println(myMethodt(10.7f));
+	  }
+}
+
