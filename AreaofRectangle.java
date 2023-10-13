08db386eb6ebb6726c5bdb1aebb46166c7461600
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-10-14 01:08:11 +0530

    Add files via upload

---
+++ AreaofRectangle.java
@@ -0,0 +1,13 @@
+package october100;
+
+public class AreaofRectangle 
+{
+	public static void main(String[] args) {
+	    int length = 5;  
+	    int width = 2;  
+	    int area = length * width;  
+	    System.out.println("Area of rectangle: " + area);  
+	  }
+	}
+
+
