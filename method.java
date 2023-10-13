fbb5ba1ffa170c7fae2d58a13f21e1405919bf7e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-10-13 22:53:55 +0530

    Add files via upload

--- method.java
+++ method.java
@@ -1,5 +1,13 @@
-package selfpratices;
+package october100;
 
-public class method {
+public class method 
+{
+	 static void myMethod() {
+		    System.out.println("I just got executed!");
+		  }
+
+		  public static void main(String[] args) {
+		    myMethod();
+		  }
 
 }
