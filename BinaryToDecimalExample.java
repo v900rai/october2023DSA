04a1b4fc05657c796b62193061b8c76615d4a9e7
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-10-18 00:26:22 +0530

    Add files via upload

---
+++ BinaryToDecimalExample.java
@@ -0,0 +1,13 @@
+package october100;
+
+public class BinaryToDecimalExample 
+{
+	public static void main(String []args)
+	{
+		//public static void main(String args[]){  
+			String binaryString="1010";  
+			int decimal=Integer.parseInt(binaryString, 2);  
+			System.out.println(decimal);  
+	}
+
+}
