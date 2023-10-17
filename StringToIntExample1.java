bc9f289e5d4e2a131d9d32ce8ff22d90424fc4fc
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-10-17 21:57:13 +0530

    Add files via upload

---
+++ StringToIntExample1.java
@@ -0,0 +1,17 @@
+package october100;
+
+public class StringToIntExample1 
+{
+	public static void main(String []args)
+	{
+		String str="200";
+		//Declaring String variable  
+		String s="200";  
+		//Converting String into int using Integer.parseInt()  
+		//int i=Integer.parseInt(s);  
+		int i=Integer.parseInt(s);
+		//Printing value of i  
+		System.out.println(i);
+	}
+
+}
