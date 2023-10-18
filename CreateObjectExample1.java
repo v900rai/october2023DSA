afa80ecaec7915cee40599aab65aab92ff01cf49
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-10-18 21:14:59 +0530

    Add files via upload

---
+++ CreateObjectExample1.java
@@ -0,0 +1,15 @@
+package october100;
+
+public class CreateObjectExample1 
+{
+	public void show()
+	{
+		System.out.println("Create object ");
+	}
+	public static void main(String []args)
+	{
+		CreateObjectExample1 obj=new CreateObjectExample1();
+		obj.show();
+	}
+
+}
