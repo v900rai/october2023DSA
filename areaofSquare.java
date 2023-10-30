04dedfe977bc8811d10acd06c8eb704b5510187e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-10-30 18:49:28 +0530

    Add files via upload

---
+++ areaofSquare.java
@@ -0,0 +1,19 @@
+package selfpratices;
+import java.util.Scanner;
+
+public class areaofSquare 
+{
+	public static void main(String []args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the side1");
+		double side1=sc.nextDouble();
+		System.out.println("Enter the side2");
+		double side2=sc.nextDouble();
+		System.out.println("total result");
+		double result=side1*side2;
+		System.out.println(result);
+		
+	}
+
+}
