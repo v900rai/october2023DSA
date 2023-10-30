43cc261fbfac3bdd5cdf12397578493a3bcd668b
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-10-30 18:50:30 +0530

    Add files via upload

---
+++ methodcalling.java
@@ -0,0 +1,23 @@
+package selfpratices;
+
+public class methodcalling 
+{
+	public static void main(String []args)
+	{
+		String x=vishal(true,100,12);
+		System.out.println(x);
+		
+	}
+	public static String vishal(boolean f, int a, int d) 
+	{
+		System.out.println(f);
+		System.out.println(a);
+		System.out.println(d);
+		return "rairai";
+	
+	}
+	
+	
+
+
+}
