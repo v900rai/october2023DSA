b22b277c46b88ddb65f0ad449ee21052bd1b6ff9
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-11-07 20:22:56 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,18 @@
+package selfpratices;
+
+public class demo
+{
+	public static void main(String []args)
+	{
+		int x=m1(10,20,"vishal");
+		System.out.println(x);
+		
+	}
+	public static int m1(int a1, int b1, String s1) {
+		
+	
+		System.out.println(a1);
+		System.out.println(b1);
+		return 100;
+	}
+}
